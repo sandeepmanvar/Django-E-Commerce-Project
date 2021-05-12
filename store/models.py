8c1dd63168eca04c990527bf0452f30d2d8892c4
Sandeep M <sandeep.manvar@outlook.com>
2021-05-12 21:24:36 +0530

    Worked on products with categories
    - create a new store app and products model
    - add products and its images
    - display products on homepage
    - create a store page and display products
    - add functionality on store page to display product by category slug

---
+++ store/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from category.models import Category
+
+# Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='images/products')
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
